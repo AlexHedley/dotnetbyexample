d43e305653ba15c03e9319e08b71df4ec6a20015
Alex Hedley <1573469+AlexHedley@users.noreply.github.com>
2025-06-06 19:18:06 +0100

    First example: Hello, World!

---
+++ examples/README.md
@@ -0,0 +1,18 @@
+# Examples
+
+Create a 📂 folder with
+
+- `.csx`
+- `.fsx`
+- `.vb`
+- `.bat`
+
+## Hello World
+
+`dotnet script .\hello-world\HelloWorld.csx`
+<!-- `dotnet script .\hello-world\HelloWorld.vb` -->
+`dotnet fsi .\hello-world\HelloWorld.fsx`
+`cd hello-world` `.\HelloWorld.bat`
+
+> [!NOTE]
+> vb scripting isn't supported
---
+++ examples/hello-world/HelloWorld.bat
@@ -0,0 +1,14 @@
+:: To run the program, put the code in `HelloWorld.csx` and
+:: use `dotnet script`.
+dotnet script HelloWorld.csx
+
+:: To run the program, put the code in `HelloWorld.vb` and
+:: use `dotnet script`.
+dotnet script HelloWorld.vb
+
+:: To run the program, put the code in `HelloWorld.fsx` and
+:: use `dotnet fsi`.
+dotnet fsi HelloWorld.fsx
+
+:: Now that we can run .NET programs, let's
+:: learn more about the language.
